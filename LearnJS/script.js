9f897959c599bdc59c42dd1e0a3b696162d79dc5
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-02 11:36:55 +0500

    book

---
+++ LearnJS/script.js
@@ -0,0 +1,14 @@
+// var age = 23;
+
+/*console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")
+console.log("jfksldjf")*/
+
+document.write("JS first class , js external file is working")
