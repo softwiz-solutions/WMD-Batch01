352846023a4f33080dd88dc5acc35efb97d12216
Abdul Hanan <abdulhanan3279@gmail.com>
2024-11-16 11:38:10 +0500

    if else

--- LearnJS/script.js
+++ LearnJS/script.js
@@ -11,7 +11,7 @@ console.log("jfksldjf")
 console.log("jfksldjf")
 console.log("jfksldjf")*/
 
-document.write("JS first class , js external file is working");
+// document.write("JS first class , js external file is working");
 
 // var age = 16;
 // var studentName = "Waqas ali ";
@@ -32,10 +32,10 @@ document.write("JS first class , js external file is working");
 // var age; // declartion
 // age = 23; // initliazation
 // var age = 23; // declaration + initlization
-var age = 23;
-console.log("age", age);
-age = 45;
-console.log("age", age);
+// var age = 23;
+// console.log("age", age);
+// age = 45;
+// console.log("age", age);
 
 // **********************Variable Naming Rules***************************
 // 1. only contains, (A-Z),(a-z),(0-9) and _
@@ -88,15 +88,15 @@ console.log("age", age);
 // console.log("result", result);
 
 // Increment / Decrement
-var n1 = 2;
-// n1 = n1 + 10;
-// n1 = n1 + 1;
+// var n1 = 2;
+// // n1 = n1 + 10;
+// // n1 = n1 + 1;
+// // console.log("n1", n1);
+// n1++; // n1=n1+1 --> n1=3
+// n1--; // n1=n1-1 --> n1=2
+// ++n1; // n1=1+n1 --> n1=3
+// --n1; // n1=1+n1 --> n1=2
 // console.log("n1", n1);
-n1++; // n1=n1+1 --> n1=3
-n1--; // n1=n1-1 --> n1=2
-++n1; // n1=1+n1 --> n1=3
---n1; // n1=1+n1 --> n1=2
-console.log("n1", n1);
 
 // prefix ++n1, postFix n1++
 
@@ -110,7 +110,79 @@ console.log("n1", n1);
 // console.log("b", b);
 // console.log("a", a);
 
-var x = 1;
-var result = x++ + x-- - x++ - ++x + ++x - x--;
-console.log("result", result);
-console.log("x", x);
+// var x = 1;
+// var result = x++ + x-- - x++ - ++x + ++x - x--;
+// console.log("result", result);
+// console.log("x", x);
+
+// 2. Comparison Operators
+
+// var number1 = 8;
+// var number2 = 8;
+// console.log("greater", number1 >= number2);
+
+// console.log("comparison", 1 == "1");
+// console.log("comparison", 1 === "1");
+// console.log("comparison", "hello how are you?" === "hello how are you?");
+// console.log("comparison", 1 != 2);
+
+// number >18 number<25
+
+// var number = 24;
+// console.log("between", number >= 18 && number <= 23);
+// // F && T --> false
+
+// var student = "#";
+// console.log("student", student == "a" || student == "b" );
+// // T || F --> T
+
+// var number = 12;
+// var result = number > 12 && !(number < 134 || number < 13);
+// F && !T ||
+// F && F --> F
+
+// var n=2;
+// var result=!n>12 && !(n < 134 || n < 13) || n > 13;
+
+// console.log("reuslt",result)
+// var number = 12;
+// // number = number + 10;
+// number += 10;
+// console.log("number", number);
+
+// var age = parseInt(prompt("Enter your age"));
+// // var age = parseInt(ageInput);
+// // var result=condition?true:false
+// console.log("age", typeof age);
+// console.log("age",  age);
+// var result = age > 18 ? "your are adult" : "you are child";
+// console.log("result", result);
+
+// marks, total maarks, percentage own,
+
+// 90 grearter A
+// 80 grearter B
+// 70 grearter C
+// 60 grearter D
+// 60 small f
+
+// var obtainedMarks = parseInt(prompt("Enter your obtained marks"));
+// var totalMarks = parseInt(prompt("Enter your total marks"));
+// var percentage = (obtainedMarks / totalMarks) * 100;
+
+// console.log("perce", percentage);
+
+// if (percentage >= 90) {
+//   console.log("A");
+// } else if (percentage >= 80) {
+//   console.log("B");
+// } else if (percentage >= 70) {
+//   console.log("C");
+// } else if (percentage >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+
+
