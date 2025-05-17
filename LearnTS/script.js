08f26d71789911fe71ce9a055d46fbcf28059f2a
Abdul Hanan <abdulhanan3279@gmail.com>
2025-05-17 11:33:53 +0500

    Feat:learn TS

---
+++ LearnTS/script.js
@@ -0,0 +1,16 @@
+console.log("Hello TS");
+console.log("Hello TSC");
+var number1 = 19;
+var number2 = 1;
+number2 = 2;
+console.log(number1 + number2);
+var userName = "John Doe";
+var userAge = 30;
+var isActive = true;
+var someValue = "Hello TypeScript";
+var strLength = someValue.length;
+console.log(strLength); // Output: 16
+var input = document.getElementById("username");
+input.value = "TypeScript user";
+var numb = 12;
+numb = "12";
---
+++ LearnTS/script.ts
@@ -0,0 +1,32 @@
+console.log("Hello TS")
+console.log("Hello TSC")
+
+
+var number1:number=19
+var number2:number=1;
+
+number2=2;
+console.log(number1+number2)
+
+
+var userName:string="John Doe"
+var userAge:number=30
+var isActive:boolean=true
+
+var someValue: any = "Hello TypeScript";
+var strLength: number = (someValue as string).length;
+console.log(strLength); // Output: 16
+
+
+
+
+const input = document.getElementById("username") as HTMLInputElement;
+input.value = "TypeScript user";
+
+
+
+var numb=12;
+
+
+
+numb="12"
