a992b04fba2f9679345ee11c2f947a6004b7340d
Abdul Hanan <abdulhanan3279@gmail.com>
2025-05-25 10:07:20 +0500

    TS interface

--- LearnTS/script.js
+++ LearnTS/script.js
@@ -1,20 +1,20 @@
-console.log("Hello TS");
-console.log("Hello TSC");
-var number1 = 19;
-var number2 = 1;
-number2 = 2;
-console.log(number1 + number2);
-var userName = "John Doe";
-var userAge = 30;
-var isActive = true;
-var someValue = "Hello TypeScript";
-var strLength = someValue.length;
-console.log(strLength); // Output: 16
-var student = {
-    name: "ali",
-    age: 20,
-    isActive: true,
-    getName: function () {
-        return this.name;
-    },
+// console.log("Hello TS")
+// console.log("Hello TSC")
+var sparrow = {
+    name: "Sparrow",
+    sound: "Chirp",
+    age: 2,
+    canFly: true,
+};
+var bulldog = {
+    name: "Bulldog",
+    sound: "Bark",
+    age: 5,
+    breed: "Bulldog",
+};
+var persianCat = {
+    name: "Persian Cat",
+    sound: "Meow",
+    age: 3,
+    numberOfLegs: "Four",
 };
--- LearnTS/script.ts
+++ LearnTS/script.ts
@@ -1,29 +1,22 @@
-console.log("Hello TS")
-console.log("Hello TSC")
+// console.log("Hello TS")
+// console.log("Hello TSC")
 
+// var number1:number=19
+// var number2:number=1;
 
-var number1:number=19
-var number2:number=1;
+// number2=2;
+// console.log(number1+number2)
 
-number2=2;
-console.log(number1+number2)
+// var userName:string="John Doe"
+// var userAge:number=30
+// var isActive:boolean=true
 
-
-var userName:string="John Doe"
-var userAge:number=30
-var isActive:boolean=true
-
-var someValue: any = "Hello TypeScript";
-var strLength: number = (someValue as string).length;
-console.log(strLength); // Output: 16
-
-
-
+// var someValue: any = "Hello TypeScript";
+// var strLength: number = (someValue as string).length;
+// console.log(strLength); // Output: 16
 
 // const input = document.getElementById("username") as HTMLInputElement;
 // input.value = "TypeScript user";
-
-
 
 // var numb=12;
 // numb="12"
@@ -32,8 +25,6 @@ console.log(strLength); // Output: 16
 //     console.log("fName",fName)
 //     console.log("number",numb)
 // }
-
-
 
 // var testParam=function (name:string,fName:string,...numb:number[]){
 //     console.log("name",name)
@@ -49,24 +40,98 @@ console.log(strLength); // Output: 16
 // var testParam= (a:number):number =>  a+b
 // var testParam= (a:number):number =>  a+1
 
-
 // console.log("test",testParam(1))
 
 // var value:string|number;
 
-interface Person{
-    name:string,
-    age:number,
-    isActive:boolean,
-    getName:()=>string
+// interface Person {
+//   name: string;
+//   age: number;
+//   isActive: boolean;
+//   getName: string[] | string | (() => string);
+// }
+
+// var student: Person = {
+//   name: "ali",
+//   age: 20,
+//   isActive: true,
+//   getName: function () {
+//     return this.name;
+//   },
+//   getName: "world"
+// };
+
+interface student {
+  [key: string]: number;
 }
 
-var student:Person={
-    name:"ali",
-    age:20,
-    isActive:true,
-    getName:function(){
-        return this.name
-    },
+// var student: student = {
+//   "math": 90,
+// };
+// console.log("student.match",student["math"])
+
+// interface CityTemp {
+//   [key: string]: number;
+// }
+// var cityTemp: CityTemp = {};
+// var cites = ["karachi", "lahore", "islamabad"];
+// for (let i = 0; i < cites.length; i++) {
+//   let value = cites[i];
+//   console.log("🚀 ~ value:", value);
+//   cityTemp[value] = Math.floor(Math.random() * 100); // create random keys of object
+// }
+// console.log("cityTemp", cityTemp);
+
+// **************************Interface and inheritance**************************
+
+// interface Person {
+//   age: number;
+//   name: string;
+//   fName: string;
+//   phoneNumber: string;
+// }
+// interface Musician extends Person {
+// //   phoneNumber: number;
+//   instrument: string;
+// }
+// var drummer: Musician = {
+//   age: 12,
+//   name: "John",
+//   fName: "Doe",
+//   instrument: "Drums",
+//   phoneNumber: "1234567890",
+// };
+
+interface Animal {
+  name: string;
+  sound: string;
+  age: number;
+}
+interface Dog extends Animal {
+  breed: string;
+}
+interface Cat extends Animal {
+  numberOfLegs: string;
+}
+interface Bird extends Animal {
+  canFly: boolean;
 }
 
+let sparrow: Bird = {
+  name: "Sparrow",
+  sound: "Chirp",
+  age: 2,
+  canFly: true,
+};
+let bulldog: Dog = {
+  name: "Bulldog",
+  sound: "Bark",
+  age: 5,
+  breed: "Bulldog",
+};
+let persianCat: Cat = {
+  name: "Persian Cat",
+  sound: "Meow",
+  age: 3,
+  numberOfLegs: "Four",
+};
